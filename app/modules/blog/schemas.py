cac7a5be2aa0f956c5c2ad5cdf987f14f2d30d26
Andrew <skkqw@yandex.ru>
2025-07-04 11:46:57 +0300

    Добавление схем

--- app/modules/blog/schemas.py
+++ app/modules/blog/schemas.py
@@ -24,6 +24,60 @@ class BlogCreateSchemaBase(BaseModelConfig):
 
 class BlogCreateSchemaAdd(BlogCreateSchemaBase):
     """
-    Схема добавления автора
+    Схема добавления автора.
     """
     author: uuid.UUID
+
+
+class UserBase(BaseModelConfig):
+    """
+    Базовая схема пользователя.
+    """
+    id: uuid.UUID
+    first_name: str
+    last_name: str
+
+
+class TagResponse(BaseModelConfig):
+    """
+    Схема тегов.
+    """
+    id: uuid.UUID
+    name: str
+
+
+class BlogFullResponse(BaseModelConfig):
+    """
+    Схема блога (полное описание).
+    """
+    id: uuid.UUID
+    author: uuid.UUID
+    title: str
+    content: str
+    short_description: str
+    created_at: datetime
+    status: str
+    tags: List[TagResponse]
+    # Это поле нужно для работы computed fields, но оно не будет включено в финальный JSON
+    user: UserBase = Field(exclude=True)
+
+    # Используем вычисляемые поля для преобразования данных о пользователе
+    @computed_field
+    @property
+    def author_id(self) -> uuid.UUID:
+        return self.user.id if self.user else None
+
+    @computed_field
+    @property
+    def author_name(self) -> str:
+        if self.user:
+            return f"{self.user.first_name} {self.user.last_name}"
+        return None
+
+
+class BlogNotFind(BaseModel):
+    """
+    Схема блога (блог не доступен).
+    """
+    message: str
+    status: str
