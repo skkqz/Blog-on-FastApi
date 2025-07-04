dd648e771e75f9a5b63592ef03335399b9dbb7c9
Andrew <skkqw@yandex.ru>
2025-07-04 08:54:37 +0300

    Добавлена if __name__ .... Для отладки ошибок

--- app/main.py
+++ app/main.py
@@ -78,3 +78,8 @@ def register_routers(app: FastAPI) -> None:
 
 # Создание экземпляра приложения
 app = create_app()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
