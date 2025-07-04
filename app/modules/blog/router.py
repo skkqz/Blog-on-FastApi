9838f01a9c82745acb635035b9e03119dd471da5
Andrew <skkqw@yandex.ru>
2025-07-04 11:49:14 +0300

    Добавление методов

--- app/modules/blog/router.py
+++ app/modules/blog/router.py
@@ -1,14 +1,15 @@
+import uuid
+
 from fastapi import APIRouter, Depends, HTTPException, status, Query
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 from fastapi.responses import JSONResponse
 
+from app.core.dependencies.blog_dep import get_blog_info
 from app.core.dependencies.dao_dep import get_session_with_commit
-# from app.api.schemas import BlogCreateSchemaBase, BlogCreateSchemaAdd, BlogFullResponse, BlogNotFind
-from app.modules.blog.schemas import BlogCreateSchemaBase, BlogCreateSchemaAdd
+from app.modules.blog.schemas import BlogCreateSchemaBase, BlogCreateSchemaAdd, BlogFullResponse, BlogNotFind
 from app.core.dependencies.auth_dep import get_current_user
-# from app.core.dependencies.auth_dep import get_current_user, get_blog_info
 from app.modules.auth.models import User
 from app.modules.blog.dao import BlogDAO, BlogTagDAO, TagDAO
 
@@ -16,7 +17,7 @@ from app.modules.blog.dao import BlogDAO, BlogTagDAO, TagDAO
 router = APIRouter()
 
 
-@router.post('/add_post/', summary="Добавление нового блога с тегами")
+@router.post('/add_post/', summary='Добавление нового блога с тегами')
 async def add_blog(
         add_data: BlogCreateSchemaBase,
         user_data: User = Depends(get_current_user),
@@ -52,3 +53,39 @@ async def add_blog(
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                                 detail='Блог с таким заголовком уже существует.')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='Ошибка при добавлении блога.')
+
+
+#/todo найти решение вывода информации о блоге
+@router.get('/get_blog/{blog_id}', summary="Получить информацию по блогу")
+async def get_blog_endpoint(
+        blog_id: uuid.UUID,
+        blog_info: BlogFullResponse | BlogNotFind = Depends(get_blog_info)
+) -> BlogFullResponse | BlogNotFind:
+    """
+    Получить блог.
+    :param blog_id: Идентификатор блога.
+    :param blog_info: Информация о блоге.
+    :return: Информация о блоге.
+    """
+
+    return blog_info
+
+
+@router.post('delete_blog/{blog_id}', summary='Удалить блог')
+async def delete_blog(
+        blog_id: uuid.UUID,
+        session: AsyncSession = Depends(get_session_with_commit),
+        current_user: User = Depends(get_current_user)
+):
+    """
+    Удаление блога.
+    :param blog_id: Идентификатор блога.
+    :param session: Сессия.
+    :param current_user: Данные пользователя.
+    :return:
+    """
+
+    result = await BlogDAO.delete_blog(session, blog_id, current_user.id)
+    if result['status'] == 'error':
+        raise HTTPException(status_code=400, detail=result['message'])
+    return result
